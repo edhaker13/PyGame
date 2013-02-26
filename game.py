3016cd91d0b8f687c42c2a910ba0c56c6fbe074b
Luis Checa <edhaker13@gmail.com>
2013-02-26 14:03:07 +0000

    Working with section updates
    Next step reposition and refine

--- game.py
+++ game.py
@@ -19,25 +19,25 @@ menu = pygame.image.load('hangman.jpg').convert()
 # Assign colour names for ease of use
 red = pygame.Color(255,0,0)
 green = pygame.Color(0,255,0)
-blue = pygame.Color(0,0,255)
-black = pygame.Color(0,0,0)
-white = pygame.Color(255,255,255)
-font = pygame.font.SysFont('calibri', 28)
+#blue = pygame.Color(0,0,255)
+#black = pygame.Color(0,0,0)
+#white = pygame.Color(255,255,255)
+font = pygame.font.SysFont('calibri', 30)
 missed = ''
 correct = ''
-guess = ''
 warning = 'Guess a letter.'
 secretWord = 'happy birthday'
 gameIsDone = False
-def draw_msg(msg):
-    msgSurface = font.render(msg, True, red)
-    msgRect = msgSurface.get_rect()
-    msgRect.topleft = (10,10)
-    window.blit(msgSurface, msgRect)
+rect_list = []
+is_valid = False
 
+def draw_bg():
+    window.blit(bg, (0,0))
+    pygame.display.update()
+    
 def draw_menu():
     window.blit(menu,(0,0))
-    draw_msg('Guess or die trying!')
+    draw_msg('Guess or die trying!', menu)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -47,36 +47,35 @@ def draw_menu():
             if event.key == K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
             if event.key == K_a:
-                displayBoard()
-    pygame.display.update()
+                return
     fps.tick(30)
+    pygame.display.update()
+    
+def draw_msg(msg, bg):
+    msgSurface = font.render(msg, True, red)
+    msgRect = msgSurface.get_rect()
+    msgRect.topleft = (10,5)
+    window.blit(bg,msgRect.topleft,msgRect)
+    window.blit(msgSurface, msgRect)
+    global rect_list
+    rect_list.append(msgRect)
+    
 def draw_font(guess, pos):
     Surface = font.render(guess, True, green)
     Rect = Surface.get_rect()
     Rect.topleft = pos
     window.blit(Surface, Rect)
-def checkGuess(alreadyGuessed):
-    if len(guess) == 0:
-        return False
-    if len(guess) != 1:
-        warning ='Please enter a single letter.'
-        return False
-    elif guess in alreadyGuessed:
-        warning = 'You have already guessed that letter. Choose again.'
-        return False
-    elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-        warning = 'Please enter a LETTER.'
-        return False
-    else:
-        return True
-def displayBoard(missed, correct, secretWord):
-    window.blit(bg, (0,0))
-
+    return Rect
+ 
+def draw_wrong(missed):
     msg = 'Missed letters:'
     for letter in missed:
         msg += letter + ", "
-    draw_font(msg, (50, 30))
+    Rect = draw_font(msg, (50, 35))
+    global rect_list
+    rect_list.append(Rect)
 
+def draw_word(secretWord, correct):
     blanks = '_' * len(secretWord)
 
     for i in range(len(secretWord)): # replace blanks with correctly guessed letters
@@ -85,52 +84,74 @@ def displayBoard(missed, correct, secretWord):
     toguess= ''
     for letter in blanks: # show the secret word with spaces in between each letter
         toguess += letter
-    draw_font(toguess, (50, 80))
-    draw_msg(warning)
+    Rect = draw_font(toguess, (50, 80))
+    global rect_list
+    rect_list.append(Rect)
 
-while True:
-    fps.tick(30)
-    displayBoard(missed, correct, secretWord)
-    pygame.display.update()
-      
-    # Let the player type in a letter.
-    while not checkGuess(missed + correct):
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-                    
-            elif event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    pygame.event.post(pygame.event.Event(QUIT))
-                guess = pygame.key.name(event.key)
-                
-        draw_msg(warning)
-        pygame.display.update()
+def check_guess(guess, alreadyGuessed):
+    if len(guess) != 1:
+        draw_msg('Please guess a letter'. bg)
+    elif guess in alreadyGuessed:
+        draw_msg('You have already guessed that letter. Choose again.', bg)
+    elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+        draw_msg('Please enter a LETTER.', bg)
+    else:
+        draw_msg('Please guess another letter', bg)
+        pygame.time.delay(50)
+        return guess
+    
+    guess = ''
+    get_input()
+    return guess
 
+def is_correct(guess,correct):
     if guess in secretWord:
         correct = correct + guess
-
+        draw_word(secretWord, correct)
         # Check if the player has won
         foundAllLetters = True
         for i in range(len(secretWord)):
             if secretWord[i] not in correct:
                 foundAllLetters = False
-                break
         if foundAllLetters:
-            draw_msg('Yes! The secret word is "' + secretWord + '"! You have won!')
+            draw_msg('Yes! The secret word is "' + secretWord + '"! You have won!', bg)
             gameIsDone = True
+        
     else:
+        global missed
         missed = missed + guess
-
+        draw_wrong(missed)
         # Check if player has guessed too many times and lost
         if len(missed) == 6:
-            displayBoard(missed, correct, secretWord)
-            draw_msg('You have run out of guesses!\nAfter ' + str(len(missed)) + ' missed guesses and ' + str(len(correct)) + ' correct guesses, the word was "' + secretWord + '"')
+            draw_msg('You have run out of guesses!', bg)
             gameIsDone = True
-
-    # Ask the player if they want to play again (but only if the game is done).
-    if gameIsDone:
-            break
+        
+def display_board(missed, correct, secretWord):
+    draw_wrong(missed)
+    draw_word(secretWord, correct)
     
+draw_bg()
+display_board(missed, correct, secretWord)
+pygame.display.update(rect_list)
+fps.tick(30)
+  
+def get_input():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+                
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+            guess = pygame.key.name(event.key)
+            guess = check_guess(guess, missed + correct)
+            return guess
+    return ''
+# Let the player type in a letter.
+while not gameIsDone:
+    rect_list = []
+    guess = get_input()
+    is_correct(guess, correct)
+    pygame.display.update(rect_list)
     
